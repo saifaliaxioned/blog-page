89104e05fe1a5968d61d7e7d85af6da454a4b6b2
saifaliaxioned <saifalis@axioned.com>
2023-01-03 19:54:32 +0530

    class added in md file

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
 module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy('css');
+
+  let markdownIt = require("markdown-it");
+  var markdownItAttrs = require('markdown-it-attrs');
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  let markdownLib = markdownIt(options).use(markdownItAttrs);
+  eleventyConfig.setLibrary("md", markdownLib);
+
   eleventyConfig.addCollection("post", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/blogs/*.md");
   });
